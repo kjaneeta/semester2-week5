428abf0b72e968e8201aaab94d051c45d14537c2
kjaneeta <jzps0355@leeds.ac.uk>
2026-03-06 16:55:26 +0000

    portfolio

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -2,24 +2,46 @@
  * Sem 2, week 5
  *
  * Portfolio submission: Hexadecimal to Decimal Converter
- * Name:
- * ID: 
+ * Name: Kayla Janeeta Rahardjo
+ * ID: 201967257
  */
 
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-	long decimal=0;
-	char hex[9];
-	
-	printf("Enter a hexadecimal:");
+int main(void) {
+    long decimal = 0;
+    char hex[9];
+
+    printf("Enter a hexadecimal:");
+    scanf("%8s", hex);
 
 	// if input contains invalid hex digit
 	// printf("Error: Invalid Hexadecimal\n");
-	
+
+    int len = strlen(hex);
+
+    for (int i = 0; i < len; i++) {
+        char c = hex[i];
+        int value;
+
+        if (c >= '0' && c <= '9') {
+            value = c - '0';
+        } else if (c >= 'a' && c <= 'f') {
+            value = c - 'a' + 10;
+        } else if (c >= 'A' && c <= 'F') {
+            value = c - 'A' + 10;
+        } else {
+            printf("Error: Invalid Hexadecimal\n");
+            return 1;
+        }
+
+        decimal = decimal * 16 + value;
+    }
+
 	// print the decimal result
-	printf("decimal:%ld\n", decimal);
-	
-	return 0;
+
+    printf("decimal:%ld\n", decimal);
+
+    return 0;
 }
